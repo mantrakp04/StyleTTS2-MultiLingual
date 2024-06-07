46aca8b90a0d0b631afeefbe937fa011e3ea5a37
Mantra <87142457+mantrakp04@users.noreply.github.com>
2024-06-07 12:46:15 +0530

    Update util.py

--- src/styletts2/Utils/PLBERT/util.py
+++ src/styletts2/Utils/PLBERT/util.py
@@ -12,32 +12,22 @@ class CustomAlbert(AlbertModel):
         return outputs.last_hidden_state
 
 
-def load_plbert(log_dir, config_path=None, checkpoint_path=None):
-    """
-
-    :param log_dir:
-    :param config_path:
-    :param checkpoint_path:
-    :return:
-    """
-    if not config_path:
-        config_path = os.path.join(log_dir, "config.yml")
+def load_plbert(log_dir):
+    config_path = os.path.join(log_dir, "config.yml")
     plbert_config = yaml.safe_load(open(config_path))
     
     albert_base_configuration = AlbertConfig(**plbert_config['model_params'])
     bert = CustomAlbert(albert_base_configuration)
 
-    if not checkpoint_path:
-        files = os.listdir(log_dir)
-        ckpts = []
-        for f in os.listdir(log_dir):
-            if f.startswith("step_"): ckpts.append(f)
+    files = os.listdir(log_dir)
+    ckpts = []
+    for f in os.listdir(log_dir):
+        if f.startswith("step_"): ckpts.append(f)
 
-        iters = [int(f.split('_')[-1].split('.')[0]) for f in ckpts if os.path.isfile(os.path.join(log_dir, f))]
-        iters = sorted(iters)[-1]
-        checkpoint_path = os.path.join(log_dir, f"step_{iters}.t7")
+    iters = [int(f.split('_')[-1].split('.')[0]) for f in ckpts if os.path.isfile(os.path.join(log_dir, f))]
+    iters = sorted(iters)[-1]
 
-    checkpoint = torch.load(checkpoint_path, map_location='cpu')
+    checkpoint = torch.load(log_dir + "/step_" + str(iters) + ".t7", map_location='cpu')
     state_dict = checkpoint['net']
     from collections import OrderedDict
     new_state_dict = OrderedDict()
@@ -46,7 +36,10 @@ def load_plbert(log_dir, config_path=None, checkpoint_path=None):
         if name.startswith('encoder.'):
             name = name[8:] # remove `encoder.`
             new_state_dict[name] = v
-    del new_state_dict["embeddings.position_ids"]
+    try:
+        del new_state_dict["embeddings.position_ids"]
+    except KeyError:
+        pass
     bert.load_state_dict(new_state_dict, strict=False)
     
     return bert
