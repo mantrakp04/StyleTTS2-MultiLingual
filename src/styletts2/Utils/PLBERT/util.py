a82c6ec4adfd49a83909c874440f1bf3a1dac668
Mantra <87142457+mantrakp04@users.noreply.github.com>
2024-06-07 13:00:06 +0530

    Update util.py

--- src/styletts2/Utils/PLBERT/util.py
+++ src/styletts2/Utils/PLBERT/util.py
@@ -2,6 +2,7 @@ import os
 import yaml
 import torch
 from transformers import AlbertConfig, AlbertModel
+from collections import OrderedDict
 
 class CustomAlbert(AlbertModel):
     def forward(self, *args, **kwargs):
@@ -12,30 +13,37 @@ class CustomAlbert(AlbertModel):
         return outputs.last_hidden_state
 
 
-def load_plbert(log_dir):
-    config_path = os.path.join(log_dir, "config.yml")
+def load_plbert(log_dir, config_path=None, checkpoint_path=None):
+    """
+    Load the pre-trained ALBERT model from the specified directory.
+
+    :param log_dir: Directory containing the model checkpoints.
+    :param config_path: Optional path to the configuration file. Defaults to "config.yml" in log_dir.
+    :param checkpoint_path: Optional path to the specific checkpoint file. If not provided, the latest checkpoint is used.
+    :return: Loaded ALBERT model.
+    """
+    if not config_path:
+        config_path = os.path.join(log_dir, "config.yml")
     plbert_config = yaml.safe_load(open(config_path))
     
     albert_base_configuration = AlbertConfig(**plbert_config['model_params'])
     bert = CustomAlbert(albert_base_configuration)
 
-    files = os.listdir(log_dir)
-    ckpts = []
-    for f in os.listdir(log_dir):
-        if f.startswith("step_"): ckpts.append(f)
+    if not checkpoint_path:
+        files = os.listdir(log_dir)
+        ckpts = [f for f in files if f.startswith("step_") and os.path.isfile(os.path.join(log_dir, f))]
+        iters = [int(f.split('_')[-1].split('.')[0]) for f in ckpts]
+        iters = sorted(iters)[-1]
+        checkpoint_path = os.path.join(log_dir, "step_" + str(iters) + ".t7")
 
-    iters = [int(f.split('_')[-1].split('.')[0]) for f in ckpts if os.path.isfile(os.path.join(log_dir, f))]
-    iters = sorted(iters)[-1]
-
-    checkpoint = torch.load(log_dir + "/step_" + str(iters) + ".t7", map_location='cpu')
+    checkpoint = torch.load(checkpoint_path, map_location='cpu')
     state_dict = checkpoint['net']
-    from collections import OrderedDict
     new_state_dict = OrderedDict()
     for k, v in state_dict.items():
         name = k[7:] # remove `module.`
         if name.startswith('encoder.'):
             name = name[8:] # remove `encoder.`
-            new_state_dict[name] = v
+        new_state_dict[name] = v
     try:
         del new_state_dict["embeddings.position_ids"]
     except KeyError:
