f9aa7f811adc7b00aeb203b6a6f4d3d8b03679de
Mantra <87142457+mantrakp04@users.noreply.github.com>
2024-06-07 13:07:11 +0530

    Update util.py

--- src/styletts2/Utils/PLBERT/util.py
+++ src/styletts2/Utils/PLBERT/util.py
@@ -46,7 +46,10 @@ def load_plbert(log_dir, config_path=None, checkpoint_path=None):
         if name.startswith('encoder.'):
             name = name[8:] # remove `encoder.`
             new_state_dict[name] = v
-    del new_state_dict["embeddings.position_ids"]
+    try:
+        del new_state_dict["embeddings.position_ids"]
+    except KeyError:
+        pass
     bert.load_state_dict(new_state_dict, strict=False)
     
     return bert
